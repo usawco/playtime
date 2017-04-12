060b040e63c6821ae9954c0bebdc8b62a5f75c65
Warren Cooper <whc.ashburn@gmail.com>
2017-04-12 12:26:58 -0400

    adding eslint and jsdoc to the mix

--- test/jenkins-test.bat
+++ test/jenkins-test.bat
@@ -1,6 +1,5 @@
 echo ==== Running tests ====
-rem TODO, checking the versions interferes w/ running the test
-rem echo node= && node -v
-rem echo npm= && npm -v
+echo node= && node -v
+echo npm= && npm -v
 echo ====
 npm run test && npm run test-jenkins
